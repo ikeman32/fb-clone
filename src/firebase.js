2887895596cd99aeac148668e03916fc196b35d9
David Isakson <ikeman32@yahoo.com>
2020-09-04 14:34:19 -0700

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC4O9yhOQWNOkOL6WdIOW4GE50yerSzIVQ",
+    authDomain: "fb-clone-db1a6.firebaseapp.com",
+    databaseURL: "https://fb-clone-db1a6.firebaseio.com",
+    projectId: "fb-clone-db1a6",
+    storageBucket: "fb-clone-db1a6.appspot.com",
+    messagingSenderId: "248393746302",
+    appId: "1:248393746302:web:58484f116cb258966c952f",
+    measurementId: "G-G60T405G69"
+  };
