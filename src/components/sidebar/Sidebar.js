d77c43e9dc7f3f663e5ad71231a0df7e124d5e10
David Isakson <ikeman32@yahoo.com>
2020-09-04 19:13:14 -0700

    Sidebar done

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './Sidebar.css';
 import { SidebarRow } from '..';
+import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags';
+import PeopleIcon from '@material-ui/icons/People';
+import ChatIcon from '@material-ui/icons/Chat';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import ExpandMoreOutlinedIcon from '@material-ui/icons/ExpandMoreOutlined';
 
 function Sidebar() {
     return (
-        <div>
-            <SidebarRow />
+        <div className='sidebar'>
+            <SidebarRow src='https://davidhisakson.com/static/media/me.b49a128f.jpg' title='UserName' />
+            <SidebarRow Icon={EmojiFlagsIcon} title='Pages' />
+            <SidebarRow Icon={PeopleIcon} title='Friends' />
+            <SidebarRow Icon={ChatIcon} title='Messenger'/>
+            <SidebarRow Icon={StorefrontIcon} title='Marketplace' />
+            <SidebarRow Icon={VideoLibraryIcon} title='Videos' />
+            <SidebarRow Icon={ExpandMoreOutlinedIcon} title='Marketplace' />
         </div>
     )
 }
